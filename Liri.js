6f8b087dfbfa71e19d2b9b50666d3500ee392688
wesley9mm <info.developer4@gmail.com>
2018-07-10 10:02:57 -0700

    add

--- Liri.js
+++ Liri.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 var keys = require("./keys.js");
-// var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
-console.log(keys);
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+console.log(keys); 
+
